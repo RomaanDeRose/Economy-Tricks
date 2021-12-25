f2da5e2c58ead8cf5585aa059283cd1da7d7cc63
Roman De Rose <romandr37@gmail.com>
2021-12-25 18:24:36 -0300

    cambios

--- js/main.js
+++ js/main.js
@@ -54,41 +54,7 @@ const allOperations = [];
 const formMoney = document.getElementById("formMoney");
 const finallyResult = document.getElementById("resultDolarOrInflation");
 
-// formMoney.addEventListener("submit", (e) => {
-// 	e.preventDefault();
-// 	const formDataOne = new FormData(e.target);
-// 	const dolarActual = 200;
-// 	const formula =
-// 		((formDataOne.get("dolar") - dolarActual) / dolarActual) * 100;
-// 	let result;
-// 	if (formula > formDataOne.get("tasa")) {
-// 		result = "Dólar";
-// 		finallyResult.innerHTML = `
-// 		<h3>Te conviene invertir en:</h3>
-// 		<p>${result}</p>
-// 		`;
-// 	} else if (formula == formDataOne.get("tasa")) {
-// 		result = "Cualquiera de los dos";
-// 		finallyResult.innerHTML = `
-// 		<h3>Te conviene invertir en:</h3>
-// 		<p>${result}! El retorno será el mismo</p>
-// 		`;
-// 	} else {
-// 		result = "Plazo Fijo";
-// 		finallyResult.innerHTML = `
-// 		<h3>Te conviene invertir en:</h3>
-// 		<p>${result}</p>
-// 		`;
-// 	}
-// 	const operationDolOrPF = new Operations("Dolar o PLazo Fijo", result);
-// 	allOperations.push(operationDolOrPF);
-// 	operationDolOrPF.save("operations", JSON.stringify(allOperations));
-// 	operationDolOrPF.saveHistorial();
-// 	formMoney.reset();
-// });
-
-// submit con JQUERY
-$("#formMoney").submit(function (e) {
+formMoney.addEventListener("submit", (e) => {
 	e.preventDefault();
 	const formDataOne = new FormData(e.target);
 	const dolarActual = 200;
@@ -97,89 +63,123 @@ $("#formMoney").submit(function (e) {
 	let result;
 	if (formula > formDataOne.get("tasa")) {
 		result = "Dólar";
-		$("#resultDolarOrInflation").append("<h3>Te conviene invertir en:</h3>");
-		$("#resultDolarOrInflation").append("<p>Dólar</p>");
+		finallyResult.innerHTML = `
+		<h3>Te conviene invertir en:</h3>
+		<p>${result}</p>
+		`;
 	} else if (formula == formDataOne.get("tasa")) {
 		result = "Cualquiera de los dos";
-		$("#resultDolarOrInflation").append("<h3>Te conviene invertir en:</h3>");
-		$("#resultDolarOrInflation").append(
-			"<p>Cualquiera de los dos! El retorno será el mismo</p>"
-		);
+		finallyResult.innerHTML = `
+		<h3>Te conviene invertir en:</h3>
+		<p>${result}! El retorno será el mismo</p>
+		`;
 	} else {
 		result = "Plazo Fijo";
-		$("#resultDolarOrInflation").append("<h3>Te conviene invertir en:</h3>");
-		$("#resultDolarOrInflation").append("<p>Plazo Fijo</p>");
+		finallyResult.innerHTML = `
+		<h3>Te conviene invertir en:</h3>
+		<p>${result}</p>
+		`;
 	}
 	const operationDolOrPF = new Operations("Dolar o PLazo Fijo", result);
 	allOperations.push(operationDolOrPF);
 	operationDolOrPF.save("operations", JSON.stringify(allOperations));
 	operationDolOrPF.saveHistorial();
-	e.target.reset();
+	formMoney.reset();
 });
+
+// // submit con JQUERY
+// $("#formMoney").submit(function (e) {
+// 	e.preventDefault();
+// 	const formDataOne = new FormData(e.target);
+// 	const dolarActual = 200;
+// 	const formula =
+// 		((formDataOne.get("dolar") - dolarActual) / dolarActual) * 100;
+// 	let result;
+// 	if (formula > formDataOne.get("tasa")) {
+// 		result = "Dólar";
+// 		$("#resultDolarOrInflation").append("<h3>Te conviene invertir en:</h3>");
+// 		$("#resultDolarOrInflation").append("<p>Dólar</p>");
+// 	} else if (formula == formDataOne.get("tasa")) {
+// 		result = "Cualquiera de los dos";
+// 		$("#resultDolarOrInflation").append("<h3>Te conviene invertir en:</h3>");
+// 		$("#resultDolarOrInflation").append(
+// 			"<p>Cualquiera de los dos! El retorno será el mismo</p>"
+// 		);
+// 	} else {
+// 		result = "Plazo Fijo";
+// 		$("#resultDolarOrInflation").append("<h3>Te conviene invertir en:</h3>");
+// 		$("#resultDolarOrInflation").append("<p>Plazo Fijo</p>");
+// 	}
+// 	const operationDolOrPF = new Operations("Dolar o PLazo Fijo", result);
+// 	allOperations.push(operationDolOrPF);
+// 	operationDolOrPF.save("operations", JSON.stringify(allOperations));
+// 	operationDolOrPF.saveHistorial();
+// 	e.target.reset();
+// });
 
 const formSalary = document.getElementById("formSalary");
 const resultSalary = document.getElementById("resultSalary");
 
 // EVENTO SUBMIT AL SEGUNDO FORMULARIO DE LA SECCION cálculos
-// formSalary.addEventListener("submit", (e) => {
-// 	e.preventDefault();
-// 	const formDataTwo = new FormData(e.target);
-// 	const salaryResult = (
-// 		formDataTwo.get("ingreso") -
-// 		(formDataTwo.get("ipc") * formDataTwo.get("ingreso")) / 100
-// 	).toFixed(2);
-// 	resultSalary.innerHTML = `
-// 	<h3>Tu Poder Adquisitivo es de:</h3>
-// 	<p>$${salaryResult}</p>
-// 	`;
-// 	const operationSalary = new Operations("Salario Real", salaryResult);
-// 	allOperations.push(operationSalary);
-// 	operationSalary.save("operations", JSON.stringify(allOperations));
-// 	operationSalary.saveHistorial();
-// 	formSalary.reset();
-// });
-
-// ANIMACIONES ANIDADAS
-function animateButton() {
-	$("#animateButton").animate(
-		{
-			width: "160px",
-		},
-		600,
-		function () {
-			$("#animateButton").animate(
-				{
-					height: "70px",
-				},
-				1200
-			);
-		}
-	);
-}
-
-const animatedButton = document.getElementById("button");
-animatedButton.addEventListener("click", (e) => {
-	e.preventDefault();
-	animateButton();
-});
-// FIN ANIMACIONES ANIDADAS
-
-// otro submit con JQUERY
-$("#formSalary").submit(function (e) {
+formSalary.addEventListener("submit", (e) => {
 	e.preventDefault();
 	const formDataTwo = new FormData(e.target);
 	const salaryResult = (
 		formDataTwo.get("ingreso") -
 		(formDataTwo.get("ipc") * formDataTwo.get("ingreso")) / 100
 	).toFixed(2);
-	$("#resultSalary").append("<h3>Tu Poder Adquisitivo es:</h3>");
-	$("#resultSalary").append(`<p>$${salaryResult}</p>`);
+	resultSalary.innerHTML = `
+	<h3>Tu Poder Adquisitivo es de:</h3>
+	<p>$${salaryResult}</p>
+	`;
 	const operationSalary = new Operations("Salario Real", salaryResult);
 	allOperations.push(operationSalary);
 	operationSalary.save("operations", JSON.stringify(allOperations));
 	operationSalary.saveHistorial();
-	e.target.reset();
+	formSalary.reset();
 });
+
+// // ANIMACIONES ANIDADAS
+// function animateButton() {
+// 	$("#animateButton").animate(
+// 		{
+// 			width: "160px",
+// 		},
+// 		600,
+// 		function () {
+// 			$("#animateButton").animate(
+// 				{
+// 					height: "70px",
+// 				},
+// 				1200
+// 			);
+// 		}
+// 	);
+// }
+
+// const animatedButton = document.getElementById("button");
+// animatedButton.addEventListener("click", (e) => {
+// 	e.preventDefault();
+// 	animateButton();
+// });
+// // FIN ANIMACIONES ANIDADAS
+
+// otro submit con JQUERY
+// $("#formSalary").submit(function (e) {
+// 	e.preventDefault();
+// 	const formDataTwo = new FormData(e.target);
+// 	const salaryResult = (
+// 		formDataTwo.get("ingreso") -
+// 		(formDataTwo.get("ipc") * formDataTwo.get("ingreso")) / 100
+// 	).toFixed(2);
+// 	$("#resultSalary").append("<h3>Tu Poder Adquisitivo es:</h3>");
+// 	$("#resultSalary").append(`<p>$${salaryResult}</p>`);
+// 	const operationSalary = new Operations("Salario Real", salaryResult);
+// 	allOperations.push(operationSalary);
+// 	operationSalary.save("operations", JSON.stringify(allOperations));
+// 	operationSalary.saveHistorial();
+// 	e.target.reset();
+// });
 
 // CARRUSEL DE ECONOMISTAS
 const carrusel = document.querySelector(".carrusel-items");
