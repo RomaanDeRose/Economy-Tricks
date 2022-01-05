f7281ca26395dc62343bae8416881962884fb117
Roman De Rose <romandr37@gmail.com>
2022-01-05 18:38:35 -0300

    cambio

--- js/main.js
+++ js/main.js
@@ -122,16 +122,16 @@ formSalary.addEventListener("submit", (e) => {
 
 // CARRUSEL DE ECONOMISTAS
 const carrusel = document.querySelector(".carrusel-items");
-let speedCarrusel = 1.5;
+let speedCarrusel = 2;
 const maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
 
 const startCarrusel = () => {
 	setInterval(() => {
 		carrusel.scrollLeft = carrusel.scrollLeft + speedCarrusel;
 		if (carrusel.scrollLeft === maxScrollLeft) {
-			speedCarrusel = 1.5;
+			speedCarrusel = -2;
 		} else if (carrusel.scrollLeft === 0) {
-			speedCarrusel = 1.5;
+			speedCarrusel = 2;
 		}
 	}, 10);
 };
